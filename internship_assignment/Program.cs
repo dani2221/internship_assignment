8483cee5d66c2bbadf798766e23397c013a8beda
Danilo <danilo.najkov@gmail.com>
2022-05-04 15:56:22 +0200

    Add project files.

---
+++ internship_assignment/Program.cs
@@ -0,0 +1,37 @@
+﻿using internship_assignment;
+using System;
+
+
+// first problem
+void AnalogueClockProblem()
+{
+    Console.WriteLine("Insert hours 0-12");
+    int hours = int.Parse(Console.ReadLine());
+    Console.WriteLine("Insert minutes 0-60");
+    int minutes = int.Parse(Console.ReadLine());
+
+    // valid check
+    if(hours < 0 || hours>11 || minutes<0 || minutes > 59)
+    {
+        Console.WriteLine("Invalid input");
+        return;
+    }
+
+    // hours / 12 gets value between 0-1, multiplied by 360 gives angle
+    float angleHours = (float) hours / 12 * 360;
+    // simmilar for minutes
+    float angleMinutes = (float) minutes / 60 * 360;
+
+    Console.WriteLine($"Lesser angle between hours and minutes is {Math.Abs(angleHours - angleMinutes)}");
+}
+
+// second and third problem
+void TreeProblem()
+{
+    Tree tree = new Tree();
+    tree.GenerateTree();
+    Console.WriteLine($"Depth of the tree is {tree.GetDepth()}");
+}
+
+AnalogueClockProblem();
+TreeProblem();
---
+++ internship_assignment/Tree.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace internship_assignment
+{
+    public class Tree
+    {
+        internal class Branch
+        {
+            public List<Branch> Children { get; set; } = new List<Branch>();
+        }
+
+        private Branch Root;
+
+        public int GetDepth()
+        {
+            return GetDepthRecursive(Root);
+        }
+
+        private int GetDepthRecursive(Branch curr)
+        {
+            // base case - current branch node is a leaf
+            if(curr.Children.Count == 0) { return 1; }
+
+            return 1 + curr.Children.Select(x => GetDepthRecursive(x)).Max();
+        }
+
+        public void GenerateTree()
+        {
+            // Generates tree as in picture
+            Root = new Branch();
+            Root.Children.Add(new Branch());
+            Root.Children[0].Children.Add(new Branch());
+            Root.Children.Add(new Branch());
+            Root.Children[1].Children.Add(new Branch());
+            Root.Children[1].Children.Add(new Branch());
+            Root.Children[1].Children.Add(new Branch());
+            Root.Children[1].Children[0].Children.Add(new Branch());
+            Root.Children[1].Children[1].Children.Add(new Branch());
+            Root.Children[1].Children[1].Children[0].Children.Add(new Branch());
+            Root.Children[1].Children[1].Children.Add(new Branch());
+        }
+    }
+}
